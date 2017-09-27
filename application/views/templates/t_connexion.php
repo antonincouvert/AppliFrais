98b94fad56058bf28e3f8ef42027bd2ca61a0bc2
antonincouvert <antocoucou@gmail.com>
2017-09-27 16:33:20 +0200

    Validation code éffectué

---
+++ application/views/templates/t_connexion.php
@@ -0,0 +1,37 @@
+﻿<?php
+	$this->load->helper('url');
+	$v_path = base_url('application/views');
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+       "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+
+	<head>
+		<title>Intranet du Laboratoire Galaxy-Swiss Bourdin</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<link href="<?php echo $v_path.'/templates/css/styles.css'?>" rel="stylesheet" type="text/css" />
+	</head>
+
+	<body>
+		<div id="page">
+			<div id="entete">
+				<img src="<?php echo $v_path.'/templates/images/logo.jpg'?>" id="logoGSB" alt="Laboratoire Galaxy-Swiss Bourdin" title="Laboratoire Galaxy-Swiss Bourdin" />
+				<h1>Suivi du remboursement des frais</h1>
+			</div>
+
+			<?php echo $body; ?>
+
+			<div id="pied">
+				<p>
+			    	<a href="http://validator.w3.org/check?uri=referer"><img
+			      	src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
+		  		</p>
+			</div>
+
+		</div>    
+
+	</body>
+</html>
+
+	  
